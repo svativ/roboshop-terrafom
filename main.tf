a99686441f8bf836c3aeffc64f682030917301fb
svati <svativangapandu9@gmail.com>
2024-01-16 21:27:16 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
-terraform {
-  backend "s3" {}
-}
-
-variable "test" {}
-output "test" {
-  value = var.test
-}
+module "components"
+  source                  =
+  for_each = var.components
+  zone_id                 = var.zone_id
+  security                = var.security_groups
+  name                    = each.value["name"]
+  instance_type           = each.value ["instance_type "]
