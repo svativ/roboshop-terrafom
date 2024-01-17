0d57d2a0f2f40eb9d80f4b763376400b4e11944b
svati <svativangapandu9@gmail.com>
2024-01-17 17:03:57 +0530

    terraform

--- vars.tf
+++ vars.tf
@@ -1,7 +1,5 @@
-variable "security_groups" {
-  default = ["sg-041edd0364521caf3"]
-}
+variable "components" {}
+variable "security_groups" {}
+variable "zone_id" {}
 
-variable "zone_id" {
-  default = "Z0018290WARH7MQ8A27A"
-}
+
