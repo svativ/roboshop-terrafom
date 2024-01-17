9b2e5e1c36cb6b3501edf2bfe6d0c2af21794129
svati <svativangapandu9@gmail.com>
2024-01-17 15:34:23 +0530

    terraform

--- env-prod/main.tf
+++ env-prod/main.tf
@@ -1,1 +1,50 @@
 test = "hello from prod"
+variable "components" {
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t3.small"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.small"
+    }
+    mongodb = {
+      name          = "mongodb"
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.small"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t3.small"
+    }
+    redis = {
+      name          = "redis"
+      instance_type = "t3.small"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.small"
+    }
+    payment = {
+      name          = "payment"
+      instance_type = "t3.small"
+    }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t3.small"
+    }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t3.small"
+    }
+    dispatch  = {
+      name          = "dispatch"
+      instance_type = "t3.small"
+
+    }
+  }
+}
