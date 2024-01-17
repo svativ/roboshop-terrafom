f982c137e034029d4ddd8f978c2bc530ff8e8990
svati <svativangapandu9@gmail.com>
2024-01-17 17:06:36 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "components" {
-  source        = "git::https://github.com/svativ/tf-module-basic-test.git"
-  for_each      = var.components
-  zone_id       = var.zone_id
-  security      = var.security_groups
-  name          = each.value["name"]
-  instance_type = each.value ["instance_type "]
+  source    = "git::https://github.com/svativ/tf-module-basic-test.git"
+  for_each  = var.components
+  zone_id          = var.zone_id
+  security         = var.security_groups
+  name             = each.value["name"]
+  instance_type    = each.value ["instance_type "]
 }
